65d460fa3672cf9e6d8ba593014e706a3afa4b8a
Rowan Austin <rowanaustin1@gmail.com>
2019-10-29 18:11:12 +0000

    Added Factorial to calc. Changed calc to static

--- Anagramo.Tests/CalculatorTests.cs
+++ Anagramo.Tests/CalculatorTests.cs
@@ -7,13 +7,82 @@ namespace Anagramo.Tests
         [Test]
         public void TestAdd()
         {
-            var calculator = new Calculator();
 
             int a = 5;
             int b = 3;
             int exp = 8;
 
-            Assert.That(calculator.Add(a,b), Is.EqualTo(exp));
+            Assert.That(Calculator.Add(a,b), Is.EqualTo(exp));
+        }
+        
+        [Test]
+        public void TestFactorialZero()
+        {
+            int a = 0;
+            int b = 0;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+        
+        [Test]
+        public void TestFactorialOne()
+        {
+            int a = 1;
+            int b = 1;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+
+        [Test]
+        public void TestFactorialTwo()
+        {
+            int a = 2;
+            int b = 2;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+
+        [Test]
+        public void TestFactorialThree()
+        {
+            int a = 3;
+            int b = 6;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+
+        [Test]
+        public void TestFactorialFour()
+        {
+            int a = 4;
+            int b = 24;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+
+        [Test]
+        public void TestFactorialFive()
+        {
+            int a = 5;
+            int b = 120;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
+        }
+
+        [Test]
+        public void TestFactorialFiftyReturnsZero()
+        {
+            int a = 50;
+            int b = 0;
+
+            Assert.That(Calculator.GenerateFactorial(a), Is.EqualTo(b));
+            
         }
     }
 }
--- Anagramo/Calculator.cs
+++ Anagramo/Calculator.cs
@@ -1,10 +1,33 @@
 namespace Anagramo
 {
-    internal class Calculator
+    internal static class Calculator
     {
-        public int Add(int a, int b)
+        public static int Add(int a, int b)
         {
             return a + b;
         }
+
+
+        public static int GenerateFactorial(int a)
+        {
+            if (0 < a && a < 50)
+            {
+                int b = 1;
+                for (int i = a; i > 1; i--)
+                {
+                    b *= i;
+                }
+                
+                return b;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
+
+
+
 }
