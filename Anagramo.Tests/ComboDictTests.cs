5f3c1b597d4ab0da5568fc3c9fbb3068d164da5c
James Smyth <jsmyth24@gmail.com>
2019-11-03 15:12:59 +0000

    ComboDict
    It's also a shitty name

---
+++ Anagramo.Tests/ComboDictTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace Anagramo.Tests
+{
+    public class ComboDictTests
+    {
+        public void CreatesCorrectKeyForThreeLetterWord()
+        {
+            string word = "Gym";
+            int key = 61 * 53 * 43;
+
+
+            Assert.That(ComboDict.GetComboKey(word) == key);
+        }
+        
+        public void CreatesCorrectKeyForSixLetterWord()
+        {
+            string word = "Weight";
+            int key = 59 * 2 * 11 * 61 * 23 * 3;
+
+            Assert.That(ComboDict.GetComboKey(word) == key);
+        }
+
+        public void CanAddWord()
+        {
+            string word = "Box";
+            int key = ComboDict.GetComboKey(word);
+            var dict = new ComboDict();
+            
+            dict.Add(word);
+            
+            Assert.That(dict.GetWordsByKey(key).Contains(word));
+        }
+
+        public void AnagrammaticWordsHaveSameKey()
+        {
+            string word1 = "God";
+            string word2 = "Dog";
+            var dict = new ComboDict();
+
+            int key1 = ComboDict.GetComboKey(word1);
+            int key2 = ComboDict.GetComboKey(word2);
+            
+            Assert.That(key1 == key2);
+        }
+
+        public void AnagrammaticWordsAreInSameList()
+        {
+            string word1 = "Thread";
+            string word2 = "Hatred";
+            var dict = new ComboDict();
+            
+            dict.Add(word1);
+            dict.Add(word2);
+            
+            Assert.That(dict.GetWordsByWord(word1).Contains(word2));
+        }
+    }
+}
---
+++ Anagramo/ComboDict.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Anagramo
+{
+    public class ComboDict
+    {
+        // Letters by frequency in English http://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+        private static Dictionary<char, int> _primeAlphabet = new Dictionary<char, int>()
+        {
+            {'e', 2},
+            {'t', 3},
+            {'a', 5},
+            {'o', 7},
+            {'i', 11},
+            {'n', 13},
+            {'s', 17},
+            {'r', 19},
+            {'h', 23},
+            {'d', 29},
+            {'l', 31},
+            {'u', 37},
+            {'c', 41},
+            {'m', 43},
+            {'f', 47},
+            {'y', 53},
+            {'w', 59},
+            {'g', 61},
+            {'p', 67},
+            {'b', 71},
+            {'v', 73},
+            {'k', 79},
+            {'x', 83},
+            {'q', 89},
+            {'j', 97},
+            {'z', 101},
+        };
+
+        private Dictionary<int, List<string>> _container = new Dictionary<int, List<string>>();
+
+        internal static int GetComboKey(string s)
+        {
+            int key = 1;
+            foreach (char c in s)
+            {
+                int prime = _primeAlphabet[c];
+                key *= prime;
+            }
+
+            return key;
+        }
+
+        public void Add(string word)
+        {
+            _container[GetComboKey(word)].Add(word);
+        }
+
+        public List<string> GetWordsByKey(int key)
+        {
+            if (_container.ContainsKey(key))
+            {
+                return _container[key];
+            }
+            else return null;
+        }
+
+        public List<string> GetWordsByWord(string word)
+        {
+            return GetWordsByKey(GetComboKey(word));
+        }
+    }
+}
